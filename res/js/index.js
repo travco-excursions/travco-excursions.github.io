4e33f62db3c818ee0d589f73a1ca5d098f930414
ibrahim-younan <ibrahimyounan1997@gmail.com>
2026-05-21 22:18:16 +0300

    update

--- res/js/index.js
+++ res/js/index.js
@@ -12,7 +12,7 @@ const router = createRouter({
 history: createWebHashHistory(), 
 scrollBehavior(to, from) { return {top: 0}  },
 routes: [
-{ path: '/', redirect: '/en/tours' },
+{ path: '/', component: () => import('../page/home.js') },
 
 
   {path: '/en', component: () => import('../page/en.js'), redirect: '/en/tours' ,children: [
